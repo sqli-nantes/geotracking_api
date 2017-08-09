1f353a2ac4ef1b662e618cf4d88d28831bcf3cbd
Johann <johann@latitude.ubuntu>
2017-08-09 17:00:43 +0200

    Update mock

--- src/main/java/archi/pole/rest/utils/Bootstrap.kt
+++ src/main/java/archi/pole/rest/utils/Bootstrap.kt
@@ -14,14 +14,20 @@ class Bootstrap {
     private val MOCK_DATA by lazy {
         val cons1 = mutableListOf<Consultant>()
         cons1.add(0, Consultant("Pierre", "Dupont"))
-        cons1.add(1, Consultant("Jean", "Dupond"))
+        cons1.add(1, Consultant("Jean", "Dho"))
+        cons1.add(2, Consultant("Carine", "Dupont"))
           val cons2 = mutableListOf<Consultant>()
-        cons2.add(0, Consultant("Lucie", "Bupont"))
-        listOf(
+        cons2.add(0, Consultant("Lucie", "Buton"))
+        val cons3 = mutableListOf<Consultant>()
+        cons3.add(0, Consultant("Jeanne", "Dart"))
+        cons3.add(1, Consultant("Herve", "Pour"))
+        val cons4 = mutableListOf<Consultant>()
+        cons4.add(0, Consultant("Gilles","Dupont"))
+                listOf(
                 Company("La Poste", "Auguste Pageot", cons1),
                 Company("IBP", "Nantes", cons2),
-                Company("SG", "Saint Herblain", cons1),
-                Company("CBP", "Nantes", cons2)
+                Company("SG", "Saint Herblain", cons3),
+                Company("CBP", "Nantes", cons4)
         )
     }
 
